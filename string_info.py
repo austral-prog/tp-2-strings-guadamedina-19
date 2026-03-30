ca065d39954e2bb6e198551a11ee6c367db0e3ed
guadamedina-19 <mgmedinarusso@mail.austral.edu.ar>
2026-03-30 18:55:22 -0300

    Update string_info.py

--- string_info.py
+++ string_info.py
@@ -3,3 +3,11 @@ def string_info():
     la palabra repetida 3 veces y decorada con '***'.
     """
     palabra = "Programacion"
+    print(f"Palabra: {palabra}")
+    print(f"Longitud: {len(palabra)}")
+    print(f"Primera letra: {palabra [0]}")
+    print(f"Ultima letra: {palabra [-1]}")
+    mensaje= palabra + "" + palabra + "" + palabra
+    print(f"Repetida: {mensaje}")
+    lenguaje = f"***{palabra}***"
+    print(f"Decorada: {lenguaje}")
